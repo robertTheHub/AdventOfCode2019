0677f3659f9a34073ea74b8e6b1632929bf91bce
robertthehub <rob_1028@hotmail.com>
2019-12-17 00:03:59 -0500

    Updated Day 16 to fix off by one issue

--- Day16/part2.py
+++ Day16/part2.py
@@ -6,25 +6,19 @@ with open("input.txt") as f:
         input_list.append(int(ch))
         extra.append(int(ch))
     offset = int(text[:7])
-for i in range(10000):
+for i in range(10000-1):
     input_list.extend(extra)
-pat = [0,1,0,-1]
+print(len(input_list), offset)
+input_list = input_list[offset:]
+input_list = input_list[::-1]
 
 for phases in range(100):
     new = []
-    for i in range(1, len(input_list)+1):
-        temp = 0
-        count = 1
-        position = 0
-        for num in input_list:
-            if (count % i) == 0:
-                position += 1
-                position = (position % len(pat))
-            temp += (num * pat[position])
-            count += 1
-
-        temp = abs(temp)%10
-        new.append(temp)
+    temp = 0
+    for num in input_list:
+        temp += num
+        temp1 = temp%10
+        new.append(temp1)
     input_list = new
     print("pulse:", phases+1)
-print(input_list[offset+1:offset+9])
+print(input_list)
